ba6a0e2893b2d962add6f408d008367624f86cb4
Brian B. Shao <brianshao9@gmail.com>
2020-09-05 16:50:43 -0500

    Initial webpage setup with help of tutorial

---
+++ app.py
@@ -0,0 +1,34 @@
+# https://code.visualstudio.com/docs/python/tutorial-flask
+from flask import Flask
+from flask import render_template
+from datetime import datetime
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+  return render_template("home.html")
+
+@app.route("/about/")
+def about():
+  return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+  return render_template("contact.html")
+
+"""
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+  return render_template(
+    "hello_there.html",
+    name=name,
+    date=datetime.now()
+  )
+
+@app.route("/api/data")
+def get_data():
+  return app.send_static_file("data.json")
+"""
